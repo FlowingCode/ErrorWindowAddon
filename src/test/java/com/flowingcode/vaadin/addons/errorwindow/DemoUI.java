6d6ea2b042e3d75d1b27681dba27c0c3d0fbc250
Martín López <mlopez@flowingcode.com>
2020-04-13 14:49:07 -0300

    - Corrected artifact id - Updated demo for showing other use cases

--- src/test/java/com/flowingcode/vaadin/addons/errorwindow/DemoUI.java
+++ src/test/java/com/flowingcode/vaadin/addons/errorwindow/DemoUI.java
@@ -31,14 +31,32 @@ public class DemoUI extends VerticalLayout {
 		Button errorButton = new Button("Throw Error", event -> {
 			Integer.parseInt("asdf");
 		});
+
+		Button throwErrorWithoutErrorHandler = new Button("Throw Error without an error handler", ev->{
+			try {
+				Integer.parseInt("asdf");
+			} catch (NumberFormatException e) {
+				ErrorManager.showError(e);
+			}
+		});
+		
+		Button throwErrorWithCustomMessage = new Button("Throw Error with custom message", ev->{
+			try {
+				Integer.parseInt("asdf");
+			} catch (NumberFormatException e) {
+				ErrorWindow w = new ErrorWindow(e, "CUSTOM ERROR MESSAGE");
+				w.open();
+			}
+		});
+
 		Button toggleProductionMode = new Button("Toggle production mode");
 		toggleProductionMode.addClickListener(ev->{
 			System.setProperty("productionMode",""+("false".equals(System.getProperty("productionMode"))));
 			Notification.show("Currently production mode is: " + System.getProperty("productionMode"));
 		});
-		add(errorButton, toggleProductionMode);
 		
-		
+		add(errorButton, throwErrorWithoutErrorHandler, throwErrorWithCustomMessage, toggleProductionMode);
+	
 	}
 	
 }
