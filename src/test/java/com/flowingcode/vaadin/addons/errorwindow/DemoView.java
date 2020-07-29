e2c9b1384910fd00bcda948c182e42de08afb56a
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2020-07-29 09:54:33 -0300

    fix(demo): correct DemoView route
    Fixes #4

--- src/test/java/com/flowingcode/vaadin/addons/errorwindow/DemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/errorwindow/DemoView.java
@@ -24,7 +24,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route()
+@Route("")
 public class DemoView extends VerticalLayout {
 
 	public DemoView() {
