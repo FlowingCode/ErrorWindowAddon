342f8fd058d68ddf24d79438dc76d4f4f5845970
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-23 11:03:16 -0300

    refactor(demo): integrate commons-demo

--- src/test/java/com/flowingcode/vaadin/addons/errorwindow/ErrorwindowDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/errorwindow/ErrorwindowDemoView.java
@@ -1,50 +1,22 @@
 package com.flowingcode.vaadin.addons.errorwindow;
 
 import com.flowingcode.vaadin.addons.DemoLayout;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.notification.Notification;
+import com.flowingcode.vaadin.addons.demo.impl.TabbedDemoImpl;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import java.awt.TextField;
 
 @SuppressWarnings("serial")
 @Route(value = "error-window", layout = DemoLayout.class)
 public class ErrorwindowDemoView extends VerticalLayout {
 
+	private static final String ERROR_DEMO = "Error Window Demo";
+	private static final String ERROR_SOURCE = "https://github.com/FlowingCode/ErrorWindowAddon/blob/master/src/test/java/com/flowingcode/vaadin/addons/errorwindow/ErrorwindowDemoView.java";
+
 	public ErrorwindowDemoView() {
-		Button errorButton = new Button("Throw Error", event -> {
-			Integer.parseInt("asdf");
-		});
-
-		Button throwErrorWithoutErrorHandler = new Button("Throw Error without an error handler", ev->{
-			try {
-				Integer.parseInt("asdf");
-			} catch (NumberFormatException e) {
-				ErrorManager.showError(e);
-			}
-		});
-		
-		Button throwErrorWithCustomMessage = new Button("Throw Error with custom message", ev->{
-			try {
-				Integer.parseInt("asdf");
-			} catch (NumberFormatException e) {
-				ErrorWindow w = new ErrorWindow(e, "CUSTOM ERROR MESSAGE");
-				w.open();
-			}
-		});
-		
-		Checkbox productionModeCb = new Checkbox("Production Mode");
-		productionModeCb.addValueChangeListener(e -> {
-			System.setProperty("productionMode", "" + productionModeCb.getValue().toString());
-			Notification.show("Currently production mode is: " + System.getProperty("productionMode"));
-		});
-
+		TabbedDemoImpl<ErrorwindowDemo> errorDemo = new TabbedDemoImpl<>(new ErrorwindowDemo(), ERROR_DEMO,
+				ERROR_SOURCE);
+		add(errorDemo);
 		setSizeFull();
-		add(productionModeCb, errorButton, throwErrorWithoutErrorHandler, throwErrorWithCustomMessage);
 	}
 
 }
