0cbbf9275dba7c287a7bbfab4338e11bbfe08f26
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-12-09 14:45:32 -0300

    chore: add license headers

--- src/test/java/com/flowingcode/vaadin/addons/errorwindow/test/LayoutTest.java
+++ src/test/java/com/flowingcode/vaadin/addons/errorwindow/test/LayoutTest.java
@@ -1,15 +1,15 @@
 /*-
  * #%L
- * TwinColGrid add-on
+ * Error Window Add-on Project
  * %%
- * Copyright (C) 2017 - 2020 Flowing Code
+ * Copyright (C) 2018 - 2020 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
